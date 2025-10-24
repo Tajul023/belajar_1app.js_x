1c1efdb9536203e18fd1fa718b1d8c2fa681604e
DanishAthayaSur2008 <danish.athaya135@smk.belajar.id>
2025-10-24 14:51:21 +0700

    upload projek

---
+++ belajar_1app.js
@@ -0,0 +1,75 @@
+// function nama() { //fungsi nama
+//     alert('Muhammad Tajul') // alert buat nampilin di pop up
+// }
+// nama();
+// //
+// //
+// function kelas() {
+//     console.log('10 X SIJA')
+// }
+// kelas();
+// //
+// //
+// function school(sekolah) {
+//     return sekolah;
+// }
+// document.write(`Saya bersekolah di ${school('SMK TI BAZMA')} <br><br>`);
+// //
+// //
+// function sapaNama(nama) { // parameternya nama
+//     console.log("selamat datang " + nama + " di SMK TI BAZMA")// isi buat di web
+// }
+
+// sapaNama("Udin");// ini berdua definisi buat parameter (nama)
+// sapaNama("Asep");
+// sapaNama("Bagus");
+// //
+// //
+// function tambah(a, b) {
+//     return a + b;
+// }
+
+// let hasil = tambah (9, 19);
+// document.write("Hasil penjumlahan adalah " + hasil);
+
+
+// function luasPersegiPanjang(panjang, lebar) {
+//     return panjang * lebar;
+// }
+
+// console.log("luas persegi panjangnya adalah " + hasil);
+//
+function halo() {
+    alert('Halo semua')
+}
+halo
+function nama(nama) {
+    return nama;
+}
+function age(umur) {
+    return umur;
+}
+function alamat(alammat) {
+    return alammat;
+}
+function school(sekolah) {
+    return sekolah;
+}
+function kelas(kelass) {
+    return kelass;
+}
+function hobi(hobii) {
+    return hobii;
+}
+function cita2(cita3) {
+    return cita3
+}
+function noHp(kontak) {
+    return kontak
+}
+document.write(`Perkenalkan nama saya ${nama("Muhammad Tajul 'Arus 'Arofy.")} <br> 
+Umur saya ${age("14 tahun.")} <br> Alamat rumah saya ${alamat("JL. Raya MAUK, Kecamatan MAUK, Desa Tegal Kunir Kidul.")} <br> 
+Hobi saya adalah ${hobi("baca sejarah kedatangan islam dan kisah kehidupan <br>yang di jalani oleh NABI MUHAMMAD ﷺ.")} <br> 
+Saya bersekolah di ${school("SMK TI BAZMA dan jurusan saya adalah SIJA.")} <br> 
+Saya kelas ${kelas("10 SMK.")}<br> 
+Cita-cita saya adalah ${cita2("DOSEN dan Pengusaha ternak.")} <br> Jika ingin informasi lengkapnya hubungi kontak ini : ${noHp("0859-5924-9123.")} <br>`);``
